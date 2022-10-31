5c763ae6b25f9352a73faa690a302dd9a7f4d99c
m-grigoriu <marcgrigoriu2003@gmail.com>
2022-10-31 17:55:19 -0400

    Removed caster, target attributes from Move class. Added tests for special methods in Battle and BattleCharacter classes.

--- src/main/java/com/mg105/entities/Move.java
+++ src/main/java/com/mg105/entities/Move.java
@@ -3,16 +3,10 @@ package com.mg105.entities;
 public class Move {
     private final int healthDamage;
     private final int damageDamage;
-    private final BattleCharacter caster;
 
-    //target might be unnecessary depending on how the combat works
-    private BattleCharacter target;
-
-    public Move(int healthDamage, int damageDamage, BattleCharacter caster) {
+    public Move(int healthDamage, int damageDamage) {
         this.healthDamage = healthDamage;
         this.damageDamage = damageDamage;
-        this.caster = caster;
-        this.target = null;
     }
 
     public int getHealthDamage() {
@@ -21,18 +15,5 @@ public class Move {
 
     public int getDamageDamage() {
         return this.damageDamage;
-    }
-
-    public BattleCharacter getCaster() {
-        return this.caster;
-    }
-
-    public BattleCharacter getTarget() {
-        return this.target;
-    }
-
-    // Not sure how the target system will work, (might be unnecessary)
-    public void setTarget(BattleCharacter target) {
-        this.target = target;
     }
 }
