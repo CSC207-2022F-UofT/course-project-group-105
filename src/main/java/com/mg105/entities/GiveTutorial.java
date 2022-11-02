253f510bf318355c024aad5171d0fa7ecdda333c
zhaoan12 <andyz.zhao@mail.utoronto.ca>
2022-11-01 21:49:10 -0400

    A class that checks that the player performs specific actions in the tutorial. The player cannot leave the starting room without completing the actions.

--- src/main/java/com/mg105/entities/GiveTutorial.java
+++ src/main/java/com/mg105/entities/GiveTutorial.java
@@ -1,29 +1,46 @@
 package com.mg105.entities;
 
+
 public class GiveTutorial {
 
-    private boolean Moved = false;
-    private boolean Attacked = false;
-    private boolean UsedItem = false;
+    private boolean moved = false;
+    private boolean attacked = false;
+    private boolean usedItem = false;
 
+    /**
+     * Set moved, attacked, usedItem to true if they have been performed by player
+     *
+     * action should be a valid action
+     *
+     * @param action  the action that has been performed
+     */
     public void ActionPerformedSetter(String action){
         if (action.equalsIgnoreCase("moved")){
-            this.Moved = true;
+            this.moved = true;
         } else if (action.equalsIgnoreCase("attacked")){
-            this.Attacked = true;
+            this.attacked = true;
         } else if (action.equalsIgnoreCase("useditem")){
-            this.UsedItem = true;
+            this.usedItem = true;
         }
 
     }
 
+    /**
+     * Set moved, attacked, usedItem to true if they have been performed by player
+     *
+     * action should be a valid action
+     *
+     * @param action  the action that is checked
+     *
+     * @return whether the player has performed each action
+     */
     public boolean ActionPerformedGetter(String action){
         if (action.equalsIgnoreCase("moved")){
-            return this.Moved;
+            return this.moved;
         } else if (action.equalsIgnoreCase("attacked")){
-            return this.Attacked;
+            return this.attacked;
         } else if (action.equalsIgnoreCase("useditem")){
-            return this.UsedItem;
+            return this.usedItem;
         }
         else {
             return false;
