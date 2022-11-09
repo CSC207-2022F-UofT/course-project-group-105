093100d43ea0340f8c028ba162a82d4af36fd507
sbrad044 <sierrabradley@rogers.com>
2022-11-09 13:17:56 -0500

    Attempt #1 at ChestItemGetter

--- src/main/java/com/mg105/entities/ChestItemGetter.java
+++ src/main/java/com/mg105/entities/ChestItemGetter.java
@@ -2,19 +2,28 @@ package com.mg105.entities;
 
 public class ChestItemGetter {
 
-    // verify chest. get chest item, delete chest item, show user item text
-
-
     public ChestItemGetter() {
 
 
     }
 
+    public void getChestItem() {
+        /**
+         * Adds the item in an adjacent chest (if one exists) to the inventory,
+         * then notifies DisplayText to show the user text describing what happened.
+         */
+        TreasureChest chest = ChestVerifier.verifyChest();
 
+        if (chest != null) {
+            Item reward = chest.open();
+            InventoryInteractor.addItem(reward);
+            TextGenerator.generateText('chest', reward);
+            //get TextGenerator to write something about how a chest was opened and this item was added to inventory
+        }
 
+        //problem??? what if there are multiple adjacent chests? how do we stop that from happening?
 
-
-
+    }
 
 
 }
