9f9887c5e519703bc9498ae6a442148bc463f171
Siddharth Gowda <siddharth@skube.xyz>
2022-11-11 12:06:57 -0500

    made changes to game state (added party and inventory attributes) to make my code eventually testable

--- src/main/java/com/mg105/entities/GameState.java
+++ src/main/java/com/mg105/entities/GameState.java
@@ -2,6 +2,22 @@ package com.mg105.entities;
 
 public class GameState {
 
-    public Room[][] map;
+    private Room[][] map;
 
+    private BattleCharacter[] party;
+
+    private Inventory inventory;
+
+    public GameState(Inventory inventory, BattleCharacter[] party){
+        this.inventory = inventory;
+        this.party = party;
+    }
+
+    public Inventory getInventory() {
+        return this.inventory;
+    }
+
+    public BattleCharacter[] getParty() {
+        return this.party;
+    }
 }
