3284ed43695c617fef5317a630a590d5e7e6ff41
Siddharth Gowda <siddharth@skube.xyz>
2022-11-16 00:18:30 -0500

    fixed item constants

--- src/main/java/com/mg105/utils/ItemConstants.java
+++ src/main/java/com/mg105/utils/ItemConstants.java
@@ -6,12 +6,12 @@ import com.mg105.entities.items.HealthPotion;
 public class ItemConstants {
 
     public static final String UPGRADE_TOKEN_NAME = "Upgrade Token";
-    public static final String UPGRADE_TOKEN_DESCRIPTION = "One use increases a " +
-        String.valueOf(HealthPotion.getHealingPoints()) + "specific stat on a specific character by one.";
+    public static final String UPGRADE_TOKEN_DESCRIPTION = "One use increases all stats on " +
+        "a specific character by one.";
 
     public static final String HEALTH_POTION_NAME = "Health Potion";
-    public static final String HEALTH_POTION_DESCRIPTION = "This potion will heal" +
-        String.valueOf(HealthPotion.getHealingPoints()) + "points to a character";
+    public static final String HEALTH_POTION_DESCRIPTION = "This potion will heal a character by" +
+        String.valueOf(HealthPotion.getHealingPoints()) + "points";
 
     // Names and items should be parallel list that where index of the name has the same index in the description
     public static final String[] ALL_ITEM_NAMES = {UPGRADE_TOKEN_NAME, HEALTH_POTION_NAME};
