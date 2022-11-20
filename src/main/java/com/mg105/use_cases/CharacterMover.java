65dbcaebdfb3f21d50b42c526513e74d06dd4048
Theodore <theo@pta.gg>
2022-11-19 23:49:02 -0500

    Make walls actually stop the player

--- src/main/java/com/mg105/use_cases/CharacterMover.java
+++ src/main/java/com/mg105/use_cases/CharacterMover.java
@@ -1,6 +1,6 @@
 package com.mg105.use_cases;
 
-import com.mg105.entities.GameState;
+import com.mg105.entities.*;
 import org.jetbrains.annotations.NotNull;
 
 import java.awt.*;
@@ -31,11 +31,43 @@ public class CharacterMover {
         // magnitude of the vector is 1 which is true iff the sum of the absolute value of the components is 1.
         assert Math.abs(direction.x) + Math.abs(direction.y) == 1;
 
-        state.currentPosition = new Point(
+        Point nextPosition = new Point(
             state.currentPosition.x + direction.x,
             state.currentPosition.y + direction.y
         );
+        boolean nextPositionValid = true;
 
-        updater.updateRoom();
+        Room room = state.getCurrentRoom();
+
+        if (nextPosition.x == 0 || nextPosition.x == 7 ||
+            nextPosition.y == 0 || nextPosition.y == 7) {
+            nextPositionValid = false;
+        }
+
+        for (Doorway doorway : room.getDoorways()) {
+            if (doorway.getPosition().equals(nextPosition)) {
+                nextPositionValid = true;
+                break;
+            }
+        }
+
+        for (TreasureChest chest : room.getChests()) {
+            if (chest.getPosition().equals(nextPosition)) {
+                nextPositionValid = false;
+                break;
+            }
+        }
+
+        for (OpponentSet opponents : room.getOpponents()) {
+            if (opponents.getPosition().equals(nextPosition)) {
+                nextPositionValid = false;
+                break;
+            }
+        }
+
+        if (nextPositionValid) {
+            state.currentPosition = nextPosition;
+            updater.updateRoom();
+        }
     }
 }
