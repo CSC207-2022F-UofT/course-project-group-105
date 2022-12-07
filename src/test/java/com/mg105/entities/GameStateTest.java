71af6336584f0bca5e02ef0c6cd4bd802a118c06
Theodore <theo@pta.gg>
2022-12-06 19:48:05 -0500

    Add some GameState tests

--- src/test/java/com/mg105/entities/GameStateTest.java
+++ src/test/java/com/mg105/entities/GameStateTest.java
@@ -1,5 +1,6 @@
 package com.mg105.entities;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.awt.*;
@@ -8,4 +9,34 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GameStateTest {
+    private Room firstRoom;
+    private Room lastRoom;
+    private WalkingCharacter walkingCharacter;
+    private GameState state;
+
+    @BeforeEach
+    void setup() {
+        firstRoom = new Room(new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+        lastRoom = new Room(new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+        walkingCharacter = new WalkingCharacter(new Point());
+        state = new GameState(new Inventory(), walkingCharacter);
+    }
+
+    @Test
+    void lastRoomFalse() {
+        state.setMap(firstRoom, lastRoom);
+        assertFalse(state.isCurrentRoomLastRoom());
+    }
+
+    @Test
+    void lastRoomTrue() {
+        state.setMap(firstRoom, lastRoom);
+        state.setCurrentRoom(lastRoom);
+        assertTrue(state.isCurrentRoomLastRoom());
+    }
+
+    @Test
+    void getWalkingCharacter() {
+        assertEquals(walkingCharacter, state.getWalkingCharacter());
+    }
 }
